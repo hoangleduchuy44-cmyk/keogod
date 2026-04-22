59e70ed6d8bd9084cc8900290abfe618969b2b9c
hoangleduchuy44-cmyk <hoangleduchuy44@gmail.com>
2026-04-22 16:05:29 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript for interactive elements can be added here
-// For example, handling clicks on "Xem Phân Tích" links
-
-document.querySelectorAll('.match a').forEach(link => {
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        alert('Phân tích chi tiết sẽ được hiển thị ở đây.');
-    });
-});
